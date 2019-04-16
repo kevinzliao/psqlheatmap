9fbde367a31a82aa2ca4ae7cf43de25cafa4fdf4
Kevin <kevin@Kevins-MacBook-Pro-2.local>
2019-04-16 15:04:45 -0500

    removed extra port declaration

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000;
 const db = require('./queries')
 
 app.use(bodyParser.json());
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
     res.json({ info: 'Node.js, Express, and Postgres API' })
 })
 */
-const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
 });
